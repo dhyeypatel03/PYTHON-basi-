175b774c7839d0b76a1470f83909ed921b2421ba
dhyeypatel <108233494+dhyeypatel03@users.noreply.github.com>
2023-08-23 23:22:04 +0530

    Add files via upload

---
+++ BASICPY.py
@@ -0,0 +1,558 @@
+# name = "dap"
+# age = 22
+# print("name") #""is ke ander jo likhe ge vo same hi aye ga
+# print(name)
+
+# print(age)
+# age2 = 22.3
+
+# print(age2)
+
+# is_adult = True
+# is_adult = False
+
+
+
+
+
+
+
+
+
+
+# name  = input("enter name : ")
+# print("hello your name is" + name) 
+
+
+# old_age  = input("enter your old age : ")
+
+# new_age = 2 + int(old_age)
+
+# print(new_age)
+
+# number = 18
+# print(float(number))
+
+
+
+
+
+
+
+
+
+# num1 = input("enter first number")
+# num2 = input("enter 2nd num")
+
+# sum = int(num1) + int(num2)
+
+# print("the sum of num1  " + str(num1) + "and num2 "+ str(num2) + " is : "+ str(sum))
+
+
+
+
+
+
+# name = "dap top"
+
+# print(name.upper())
+# print(name)
+
+# print(name.find("top"))
+
+# print(name.replace("dap top" , "ddaappttoopp"))
+
+# print("dao" in name)
+
+
+
+
+
+
+# print(5+2)
+# print(5+2.5464864)
+# print(5%2)
+
+
+# print(5/2)      # output 2.5
+# print(5//2)     # output 2
+
+
+# print(5*2)
+# print(5-2)
+
+
+# print(5**3)  #5 cube
+
+# i = 5
+# i = i+2
+# print(i)
+# # or
+# i += 2
+# print(i)
+
+
+# RESULT = 2 * 3 % 4 / 3  + 34
+# print(RESULT)
+
+
+
+
+
+
+
+
+
+
+# COMPARASION OPERATOR
+
+# print(3>=3)
+
+# print (3 == 3 )
+
+# print(3 != 2)
+
+# age = int(input("Enter your age"))
+# if age >= 18:
+#     print("adult")
+# else:
+#     print("child")
+
+# i = 1111
+# while i <=10000000:
+#     print(i)
+#     i+=1
+
+
+
+
+
+
+
+
+# print(frits)
+# frits.insert(2,"daptop")
+# print(frits)
+# frits = ["apple", "banana", "mango"]
+# frits.append("hello")
+# print(frits)
+# frits.extend(["laptop", "adit"])  # Use a list to extend the original list
+# print(frits)
+
+
+
+# print(["dap"]*1000000)
+
+# fruits.remove("banana")
+# print(fruits)
+# fruits = ["apple","banana","mango"]
+# fruits.pop(1)
+# print(fruits)
+# del fruits
+# print(fruits)
+
+# fruits = ["apple","banana","mango"]
+# fruits.clear()
+# print(fruits)
+
+
+# for name in ["Joe", "Amy", "Brad", "Angelina", "Zuki", "Thandi", "Paris"]:
+#     print("Hi ", name, "	Please come to my party on Saturday!")
+# else:
+#     print("No names left.")
+
+
+
+# name = ["daptop","kiran","meet","rajil","nachiket","het"]
+
+# for n in name:
+#     print("hii " , n , " please come to my party")
+# else:
+#     print("all msg sent")
+
+
+
+
+
+
+# x = range(10,10000,51)
+# for n in x:
+#     print(n)
+
+# print(list(range(2, 20, 3)))
+# print(list(range(0,1000,10)))
+
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:	
+#     print(x)
+
+# `
+# fruits = ["apple", "banana", "cherry"]
+# for i in range(len(fruits)):	#i will take values: 	
+#     print(fruits[i])
+
+
+# val = range(1,11)
+# for x in val:
+#     print(x)
+
+
+
+
+# num = int(input("enter the number of which factorial you want: "))
+# unum =num
+# facto = 1
+# while num>0:
+#     facto = facto * num
+#     num = num-1
+
+# print("factorial of the number " ,unum, " is ", facto)
+
+# for i in range(1,6):
+#     for j in range (1 , i+1):
+#         print(j, end ="")
+#     print()
+
+
+
+
+
+
+# for val in "string":
+#       if val == "n":
+#         break
+# print(val)
+
+
+
+# for val in "string":
+#       if val == "n":
+#         continue
+# print(val)
+
+
+# fruits = ["apple", "banana", "cherry"]
+# i = 0
+# while i < len(fruits):
+#       print(fruits[i])
+#       i = i + 1
+
+
+
+
+# int_val = [1,2435,2,134,564,345,435,7,2345,435,82,345,3456123,45,67,67,3,46,56,7,6,67]
+
+# int_val.sort()
+# print(int_val)
+
+# int_val.sort(reverse=True)
+# print(int_val)
+
+
+
+# fruits = ['apple', 'banana', 'cherry']
+# fruits.reverse()
+# print(fruits)
+
+
+
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+# thislist[2]="bit"
+# print(mylist)
+# print(thislist)
+
+
+
+
+
+
+
+
+
+
+# tuple1= ("dap", 1,2,33.3,"top")
+# print(type(tuple1[-3]))
+
+# tuple2 = (45,)
+# print (type(tuple2))
+
+# tuple2[0]=13     # not possible
+# print(tuple2)
+
+
+# tuple3 =(1,1,1,1)  # duplication is allowed
+# print(tuple3)
+
+
+
+
+# tuple4 =( 12,14,116,-2345,"daptop","tooper")
+# print(len(tuple4))
+
+# tuple5 = (tuple4,"dappp",123,23)
+# print(len(tuple5))
+
+
+
+# tuple6 = ("a","b","c")
+# tuple7 = (1,1,1,1,1,1,1,1,1,1,1,1,2,3,4,5)
+
+# tuple8 = (tuple6 + tuple7)
+# print(tuple8)
+
+# print(min(tuple7))
+# print(max(tuple7))
+# print(tuple7.count(1))
+# print(tuple7.index(5))
+
+
+# lista = ['a','b','c']
+# print(tuple(lista))
+
+
+# print("--------------------------LEARNING SETS------------------------")
+
+# set1 ={True,2,52,-23,45,22222,-2345,52}
+# print(set1)
+# print(set1)
+# print(set1)
+# print(set1)
+# print(set1)
+
+# set2 = {1,2,3,True,False}  #True is t printed as 1 i sTrue
+
+# print(set2)
+
+# set3 = set()
+# print(set3)
+# print(type(set1))
+# print(type(set2))
+# print(type(set3))
+
+# # set1[2]=00 not possible
+
+# set3.add(99) #one at a time
+# print(set3) 
+
+# set1.remove(1)
+# print(set1)
+
+
+# quantity =3
+# itemno =567
+# price =49
+# myorder ="I want to pay {2:.2f} dollars for {0} pieces of item {1}."
+# print(myorder.format(quantity,itemno, price))
+
+# print("C:\\Python32\\Lib")
+
+# txt ="Hello welcome to my world."
+# x = txt.find("to")
+# print(x)
+
+# txt = "Hello, welcome to my world."
+# x = txt.find("e", 5, 10)
+# print(x)
+
+# numList = ['1', '2', '3', '4']
+# separator = ', '
+# print(separator.join(numList))
+
+
+# s1 = 'abc'
+# s2 = '123'
+# print('s1.join(s2):', s1.join(s2))
+# print('s2.join(s1):', s2.join(s1))
+
+# set1 = {"a", "b" , "c"}
+# set2 = {1, 2, 3}
+# set1.update(set2)
+# print(set1)
+
+
+
+
+
+
+# print("=======================DICTIONARY==========================")
+
+# phone = {
+#             "dap":123,
+#             "top":1234,
+#             "hello":2345,
+#             "dap":1234
+#             }
+# print(phone)
+# print(phone['dap'])
+
+# phone = dict({
+#     "dap":123,
+#     "top":1234
+# })
+# print(phone)
+
+# phone = {
+#             "dap":123,
+#             "top":1234,
+#             "hello":2345,
+#             "dap":1234
+#             }
+# print(phone)
+# print(phone['dap'])
+
+# phone['dap']=1212
+# print(phone)
+# phone['top']={'top_home':840 , 'top_own':720}
+# print(phone)
+# phone2 = {
+#             "yt":990,
+#             "toooper":330 
+# }
+
+# print(phone2.get('yt'))
+
+
+# student_data =  {
+
+#                     "dhyey" : {"roll_num":10,"age":22,"course":'java'},
+#                     "daptop" : {"roll_num":11,"age":2,"course":'cpp'} 
+# }
+# print(student_data['dhyey'])
+# print(student_data['daptop']['roll_num'])
+# 
+# student_data['daptop']['phone'] = 99999
+# print(student_data["daptop"]['phone'])
+
+# del student_data['daptop']['phone']
+# print(student_data["daptop"])
+
+
+# def fibbo(n):
+#     if(n==0):
+#         return 0
+#     elif(n==1):
+#         return 1
+#     else:
+#         return (fibbo(n-1) + fibbo(n-2))
+
+# print("Fibonacci series")
+# n = int(input("enter the number of which fibonacci sequence you want: "))
+
+# for i in range(n):
+#     print(fibbo(i), end = "")
+
+
+# class student_info:
+#     name = "NA"
+#     age = "NA"
+#     mob = "not provide"
+#     def info(self):
+#         print("My name is " , self.name , " age is " , self.age , " and my mobile number is : ",self.mob)
+    
+# a = student_info()
+# a.name="dap"
+# a.age=20
+# a.mob=7203964176
+
+# b=student_info()
+# a.info()
+# b.info()
+
+
+
+
+# class bank_acc:
+#     def __init__(self , acc_no , balance):
+#         self.acc_no = acc_no
+#         self.balance = balance
+
+#     def balance(self):
+#         return self.balance
+    
+#     def deposit(self, amount):
+#         self.balance = self.balance + amount
+
+#     def withdraw(self , amount):
+#         if(amount >= self.balance):
+#             print('insufficient funds')
+#         else:
+#             self.balance -= amount
+        
+    
+
+# account = bank_acc("10293",15000)
+# account.deposit(120973)
+# account.withdraw(234)
+# account.withdraw(10000000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''create a dictionary where keys are name of student and values are another diciionary conaining sem,age,and cpiu of that student
+print all the name of student
+print the nae of student with highest cpi'''
+
+# student_info = {
+
+#                 "dhyey" : {"sem":3 , "age":20 , "cpi":9.5},
+#                 "dap" : {"sem":3 , "age":21 , "cpi":9.4},
+#                 "top" : {"sem":3 , "age":21 , "cpi":9.52},
+#                 "het" : {"sem":3 , "age":20 , "cpi":8.5},
+# }
+
+# for name in  student_info:
+#     print(name)
+
+# highest_cpi = 0
+# highest_cpi_student = ""
+
+# for name,data in student_info.items():
+#     if data['cpi']>highest_cpi:
+#         highest_cpi=data["cpi"]
+#         highest_cpi_student = name
+
+# print(highest_cpi_student)
+
+
+
+
+
+
+'''consider a list consisting the marks of python subject 
+of 10 students use reduce
+ and lambda function to calculate the average from the lsit'''
+
+# from functools import reduce
+# python_marks = [67,46,87,99,66,87,67,88,89,43]
+
+# total_marks = reduce(lambda x,y :x+y , python_marks)
+
+# average = total_marks/len(python_marks)
+
+# print(python_marks)
+# print("avg",average)
